671fd4dd3eccd3e019ab5b631a5ccb8028ebb66e
arturoricome-ship-it <arturoricome@gmail.com>
2026-08-10 04:56:04 +0200

    Abrir ajustes de la app para desinstalar de forma fiable

--- android/cta-alarm-minimal/app/src/main/java/com/cta/alarm/minimal/UninstallActivity.java
+++ android/cta-alarm-minimal/app/src/main/java/com/cta/alarm/minimal/UninstallActivity.java
@@ -4,14 +4,17 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 
 public class UninstallActivity extends Activity {
     @Override protected void onCreate(Bundle b){
         super.onCreate(b);
         try{
-            Intent i=new Intent(Intent.ACTION_DELETE, Uri.parse("package:"+getPackageName()));
+            Intent i=new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:"+getPackageName()));
             startActivity(i);
-        }catch(Exception ignored){}
+        }catch(Exception e){
+            try{startActivity(new Intent(Settings.ACTION_SETTINGS));}catch(Exception ignored){}
+        }
         finish();
     }
 }
